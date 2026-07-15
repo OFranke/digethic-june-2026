303a43cf85002a7cca27365059a5a102c529ca5c
Oliver Franke <oliver.franke@limebit.de>
2026-07-15 14:28:24 +0200

    DIG-830: add prediction endpoint

--- src/api.py
+++ src/api.py
@@ -1,6 +1,7 @@
-from flask import Flask, Response
+from flask import Flask, Response, request
 import pandas as pd
 import os
+import pickle
 
 app = Flask(__name__)
 
@@ -19,3 +20,25 @@ def hello_world():
 def training_data():
     data = pd.read_csv(os.path.join("data", "auto-mpg.csv"), sep=";")
     return Response(data.to_json(), mimetype="application/json")
+
+
+@app.route("/predict")
+def predict():
+    file_to_open = open(os.path.join("data", "models", "baummethoden_lr.pickle"), "rb")
+
+    trained_model = pickle.load(file_to_open)
+    file_to_open.close()
+
+    # load data that we want predictions for
+
+    zylinder = float(request.args.get("zylinder"))
+    ps = float(request.args.get("ps"))
+    gewicht = float(request.args.get("gewicht"))
+    beschleunigung = float(request.args.get("beschleunigung"))
+    baujahr = float(request.args.get("baujahr"))
+
+    predictions = trained_model.predict(
+        [[zylinder, ps, gewicht, beschleunigung, baujahr]]
+    )
+
+    return {"result": predictions[0]}
