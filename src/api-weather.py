8c4bfff310c8d551230100521889637aff976c21
Oliver Franke <oliver.franke@limebit.de>
2026-07-15 13:16:32 +0200

    DIG-827: add weather forecast

--- src/api-weather.py
+++ src/api-weather.py
@@ -1,3 +1,5 @@
+import requests
+
 latitude = 52.5244
 longitude = 13.4105
 
@@ -11,3 +13,26 @@ print(
     + ", "
     + str(longitude)
 )
+
+forecast_params = {
+    "latitude": latitude,
+    "longitude": longitude,
+    "daily": "temperature_2m_max",
+}
+
+
+forecast_url = "https://api.open-meteo.com/v1/forecast"
+
+response = requests.get(forecast_url, params=forecast_params)
+
+data = response.json()
+# print(data)
+
+for i in range(len(data["daily"]["time"])):
+    print(
+        "Um "
+        + str(data["daily"]["time"][i])
+        + " Uhr "
+        + str(data["daily"]["temperature_2m_max"][i])
+        + "°C"
+    )
